25313945029a5d510370656e62d3f652285cc324
YuhangWu <yuhang.wu@xyzrobotics.ai>
2024-03-14 09:58:44 +0800

    feat: 新增 Python 脚本，用于在本地启动 server

---
+++ server/im-server.py
@@ -0,0 +1,37 @@
+import socket
+import subprocess
+from datetime import datetime
+
+
+def main(port=23333):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("0.0.0.0", port))
+    server_socket.listen(1)
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established.")
+
+        while True:
+            data = client_socket.recv(1024).decode("utf-8")
+            if data == "":
+                continue
+            elif data == "exit":
+                break
+            try:
+                output = subprocess.check_output(
+                    data, shell=True, stderr=subprocess.STDOUT, timeout=10
+                )
+                client_socket.send(output)
+            except subprocess.CalledProcessError as e:
+                error_msg = f"{datetime.now()} | Failed to execute command: {data}\n{e.output.decode('utf-8')}"
+                print(error_msg)
+                client_socket.send(error_msg.encode("utf-8"))
+
+        client_socket.close()
+
+    server_socket.close()
+
+
+if __name__ == "__main__":
+    main()
