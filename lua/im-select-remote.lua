70c34b4303bf3e0d36f3e50c2fb811b85045c82b
lhybdv <39660787@qq.com>
2024-12-09 19:55:16 +0800

    feat: remove unused code

--- lua/im-select-remote.lua
+++ lua/im-select-remote.lua
@@ -1,31 +1,12 @@
 local M = {}
 M.config = {
-  osc = {
-    secret = "",
-  },
   socket = {
+    host = "127.0.0.1",
     port = 23333,
     max_retry_count = 3,
-    command = "im-select com.apple.keylayout.ABC",
+    command = "im-select 1033",
   },
 }
-
-local retry_count = 0
-
---- write writes the OSC 1337 escape sequence to the terminal
--- @tparam osc1337 string the OSC 1337 escape sequence
--- @treturn bool whether the write was successful
-local function write(osc1337)
-  local success = false
-  if vim.fn.has("nvim") then
-    success = vim.fn.chansend(vim.api.nvim_get_var("stderr"), osc1337) > 0
-  else
-    vim.cmd("silent! !echo " .. vim.fn.shellescape(osc1337))
-    vim.cmd("redraw!")
-    success = true
-  end
-  return success
-end
 
 --- check_auto_enable_conditions checks whether the auto enable conditions are met
 -- @treturn bool whether the auto enable conditions are met
@@ -36,19 +17,16 @@ local function check_auto_enable_socket()
   return false
 end
 
-M.IMSelectByOSC = function()
-  write("\033]1337;Custom=id=" .. M.config.osc.secret .. ":im-select\a")
-end
-
 --- IMSelectBySocket
 -- @treturn int the exit code of the command
 M.IMSelectBySocket = function()
   local function on_stdout() end
-  local cmd = "echo "
-    .. vim.fn.shellescape(M.config.socket.command)
-    .. " | nc localhost "
+  local cmd = "echo \""
+    .. M.config.socket.command
+    .. "\" | nc "
+    .. M.config.socket.host
+    .. " "
     .. M.config.socket.port
-    .. " -q 0"
   vim.fn.jobstart(cmd, {
     on_stdout = on_stdout,
     on_stderr = on_stdout,
@@ -56,16 +34,6 @@ M.IMSelectBySocket = function()
     stdout_buffered = false,
     stderr_buffered = false,
   })
-end
-
-M.IMSelectOSCEnable = function()
-  vim.cmd([[
-      augroup im_select_remote
-        autocmd!
-        autocmd BufEnter * lua require("im-select-remote").IMSelectByOSC()
-        autocmd InsertLeave * lua require("im-select-remote").IMSelectByOSC()
-      augroup END
-    ]])
 end
 
 M.IMSelectSocketEnable = function()
@@ -89,23 +57,7 @@ end
 
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
-  if check_auto_enable_socket() then
-    for i = 1, M.config.socket.max_retry_count do
-      local result = os.execute("nc -z localhost " .. M.config.socket.port .. " -q 0")
-      if result == 0 then
-        break
-      end
-      retry_count = i
-      vim.cmd("sleep 50m")
-    end
-
-    if retry_count == M.config.socket.max_retry_count then
-      vim.notify("IMSelectServer is not running, please start it first!", vim.log.levels.WARN)
-      return
-    end
-
-    M.IMSelectSocketEnable()
-  end
+  M.IMSelectSocketEnable()
 end
 
 return M
--- server/im-server.py
+++ server/im-server.py
@@ -16,24 +16,21 @@ def main(port=23333):
         client_socket, addr = server_socket.accept()
         logger.info(f"Connection from {addr} has been established.")
 
-        while True:
-            data = client_socket.recv(1024).decode("utf-8")
-            if data == "exit":
-                break
-            try:
-                output = subprocess.check_output(
-                    data, shell=True, stderr=subprocess.STDOUT, timeout=10
-                )
-                client_socket.send(output)
-            except subprocess.CalledProcessError as e:
-                error_msg = f"{datetime.now()} | Failed to execute command: {data}\n{e.output.decode('utf-8')}"
-                logger.error(error_msg)
-                client_socket.send(error_msg.encode("utf-8"))
-
-        client_socket.close()
+        data = client_socket.recv(1024).decode("utf-8")
+        data = data.rstrip()
+        try:
+            subprocess.check_output(
+                data, shell=True, stderr=subprocess.STDOUT, timeout=10
+            )
+        except subprocess.CalledProcessError as e:
+            error_msg = f"{datetime.now()} | Failed to execute command: {data}\n{e.output.decode('utf-8')}"
+            logger.error(error_msg)
+            client_socket.send(error_msg.encode("utf-8"))
+        finally:
+            client_socket.close()
 
     server_socket.close()
 
 
 if __name__ == "__main__":
-    main(22222)
+    main()
